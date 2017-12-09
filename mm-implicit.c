1e4a3df7d49fa3f3bbdef2346fcf57187e1e0e79
jongkukpark <trly12@cnu.ac.kr>
2017-12-09 23:37:23 +0900

    start git

---
+++ mm-implicit.c
@@ -0,0 +1,136 @@
+/*
+ * mm-implicit.c - an empty malloc package
+ *
+ * NOTE TO STUDENTS: Replace this header comment with your own header
+ * comment that gives a high level description of your solution.
+ *
+ * @id : 201302405
+ * @name : 박종국
+ */
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "mm.h"
+#include "memlib.h"
+
+/* If you want debugging output, use the following macro.  When you hand
+ * in, remove the #define DEBUG line. */
+#define DEBUG
+#ifdef DEBUG
+# define dbg_printf(...) printf(__VA_ARGS__)
+#else
+# define dbg_printf(...)
+#endif
+
+
+/* do not change the following! */
+#ifdef DRIVER
+/* create aliases for driver tests */
+#define malloc mm_malloc
+#define free mm_free
+#define realloc mm_realloc
+#define calloc mm_calloc
+#endif /* def DRIVER */
+
+/* single word (4) or double word (8) alignment */
+#define ALIGNMENT 8
+
+/* rounds up to the nearest multiple of ALIGNMENT */
+#define ALIGN(p) (((size_t)(p) + (ALIGNMENT-1)) & ~0x7)
+
+
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+#define SIZE_PTR(p)  ((size_t*)(((char*)(p)) - SIZE_T_SIZE))
+
+/*
+ * Initialize: return -1 on error, 0 on success.
+ */
+int mm_init(void) {
+    return 0;
+}
+
+/*
+ * malloc
+ */
+void *malloc (size_t size) {
+return NULL;
+}
+
+/*
+ * free
+ */
+void free (void *ptr) {
+    if(!ptr) return;
+}
+
+/*
+ * realloc - you may want to look at mm-naive.c
+ */
+void *realloc(void *oldptr, size_t size) {
+  size_t oldsize;
+  void *newptr;
+
+  /* If size == 0 then this is just free, and we return NULL. */
+  if(size == 0) {
+    free(oldptr);
+    return 0;
+  }
+
+  /* If oldptr is NULL, then this is just malloc. */
+  if(oldptr == NULL) {
+    return malloc(size);
+  }
+
+  newptr = malloc(size);
+
+  /* If realloc() fails the original block is left untouched  */
+  if(!newptr) {
+    return 0;
+  }
+
+  /* Copy the old data. */
+  oldsize = *SIZE_PTR(oldptr);
+  if(size < oldsize) oldsize = size;
+  memcpy(newptr, oldptr, oldsize);
+
+  /* Free the old block. */
+  free(oldptr);
+
+  return newptr;
+}
+
+/*
+ * calloc - you may want to look at mm-naive.c
+ * This function is not tested by mdriver, but it is
+ * needed to run the traces.
+ */
+void *calloc (size_t nmemb, size_t size) {
+    return NULL;
+}
+
+
+/*
+ * Return whether the pointer is in the heap.
+ * May be useful for debugging.
+ */
+static int in_heap(const void *p) {
+    return p < mem_heap_hi() && p >= mem_heap_lo();
+}
+
+/*
+ * Return whether the pointer is aligned.
+ * May be useful for debugging.
+ */
+static int aligned(const void *p) {
+    return (size_t)ALIGN(p) == (size_t)p;
+}
+
+/*
+ * mm_checkheap
+ */
+void mm_checkheap(int verbose) {
+}
