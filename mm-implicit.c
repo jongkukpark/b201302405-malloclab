42782d45521a27de16dbfe48e43b7f3494ada810
jongkukpark <trly12@cnu.ac.kr>
2017-12-10 00:12:22 +0900

    1

--- mm-implicit.c
+++ mm-implicit.c
@@ -46,11 +46,54 @@
 
 #define SIZE_PTR(p)  ((size_t*)(((char*)(p)) - SIZE_T_SIZE))
 
+#define WSIZE 4
+
+#define DSIZE 8
+
+#define CHUNKSIZE(1<<12)
+
+#define OVERHEAD 8
+
+#define OVERHEAD 8
+
+#define MAX(x,y) ((x) > (y) ? (x) : (y))
+
+#define PACK(size, alloc) ((size) | (alloc))
+
+#define GET(p) (*(size_t*)(p))
+
+#define PUT(p, val) (*(size_t*)(p) = (val))
+
+#define GET_SIZE(p) (GET(p) & ~0x7)
+
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+#define HDRP(bp) ((char*)(bp) - WSIZE)
+
+#define FTRP(bp) ((char*)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+#define NEXT_BLKP(bp) ((char*)(bp) + GET_SIZE((char*)(bp - WSIZE))
+
+#define PREV_BLKP(bp) ((char*)(bp) - GET_SIZE((char*)(bp) - DSIZE))
+
+static char *heap_listp = 0;
 /*
  * Initialize: return -1 on error, 0 on success.
  */
 int mm_init(void) {
-    return 0;
+   if ((heap_listp = mem_sbrk(4 * WSIZE)) == NULL)
+   		return -1;
+
+	PUT(heap_listp, 0);
+	PUT(heap_listp + WSIZE, PACK(OVERHEAD, 1));
+	PUT(heap_listp + DSIZE, PACK(OVERHEAD, 1));
+	PUT(heap_listp + WSIZE + DSIZE, PACK(0, 1);
+	heap_listp += DSIZE;
+
+	if ((exted_heap(CHUNKSIZE / WSIZE)) == NULL)
+		return -1;
+
+	return 0;
 }
 
 /*
@@ -64,7 +107,14 @@ return NULL;
  * free
  */
 void free (void *ptr) {
-    if(!ptr) return;
+    if (bp == 0)
+		return;
+	size_t size = GET_SIZE(HDRP(bp));
+
+	PUT(HERP(bp), PACK(size, 0));
+	PUT(FTRP(bp), PACK(size, 0));
+
+	coalesce(bp);
 }
 
 /*
@@ -133,4 +183,27 @@ static int aligned(const void *p) {
  * mm_checkheap
  */
 void mm_checkheap(int verbose) {
+}
+
+static void *coalesce(void * bp) {
+	size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+	size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+	size_t size = GET_SIZE(HDRP(bp));
+
+	if (prev_alloc && next_alloc) {
+		return bp;
+	}
+	else if (prev_alloc && !next_alloc) {
+		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+		PUT(HDRP(bp), PACK(size, 0));
+		PUT(FTRP(bp), PACK(size, 0));
+	}
+	else if() {
+
+	}
+	else {
+
+	}
+
+	return bp;
 }
