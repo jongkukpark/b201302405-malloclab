871bca438df5234845be2af2efc86676c38bee81
jongkukpark <trly12@cnu.ac.kr>
2017-12-10 20:39:44 +0900

    4

--- mm-implicit.c
+++ mm-implicit.c
@@ -44,7 +44,7 @@
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-#define SIZE_PTR(p)  ((size_t*)(((char*)(p)) - SIZE_T_SIZE))
+#define SIZE_PTR(p)  ((size_t *)(((char*)(p)) - SIZE_T_SIZE))
 
 #define WSIZE 4
 #define DSIZE 8
@@ -52,41 +52,44 @@
 
 #define OVERHEAD 8
 
-#define MAX(x,y) ((x) > (y) ? (x) : (y))
-
 #define PACK(size, alloc) ((size) | (alloc))
 
-#define GET(p) (*(unsigned int *)(p))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+#define GET(p)	(*(unsigned int *)(p))
 #define PUT(p, val) (*(unsigned int *)(p) = (val))
 
 #define GET_SIZE(p) (GET(p) & ~0x7)
-
 #define GET_ALLOC(p) (GET(p) & 0x1)
 
-#define HDRP(bp) ((char * )(bp) - WSIZE)
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
 
-#define FTRP(bp) ((char * )(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
-
-#define NEXT_BLKP(bp) ((char * )(bp) + GET_SIZE((char * )(bp - WSIZE))
-
-#define PREV_BLKP(bp) ((char * )(bp) - GET_SIZE((char * )(bp) - DSIZE))
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
 
 static char *heap_listp = 0;
 static void *coalesce(void *bp);
+static void *extend_heap(size_t words);
+static void *find_fit(size_t asize);
+static void place(void *bp, size_t asize);
+static int in_heap(const void *p);
+static int aligned(const void *p);
+
 /*
  * Initialize: return -1 on error, 0 on success.
  */
 int mm_init(void) {
-   if ((heap_listp = mem_sbrk(4 * WSIZE)) == NULL)
+   if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *) -1)
    		return -1;
 
 	PUT(heap_listp, 0);
-	PUT(heap_listp + WSIZE, PACK(OVERHEAD, 1));
-	PUT(heap_listp + DSIZE, PACK(OVERHEAD, 1));
-	PUT(heap_listp + WSIZE + DSIZE, PACK(0, 1));
+	PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));
+	PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));
+	PUT(heap_listp + (3*WSIZE), PACK(0, 1));
 	heap_listp += DSIZE;
 
-	if ((exted_heap(CHUNKSIZE / WSIZE)) == NULL)
+	if ((extend_heap(CHUNKSIZE / WSIZE)) == NULL)
 		return -1;
 
 	return 0;
@@ -124,9 +127,9 @@ void *malloc (size_t size) {
  * free
  */
 void free (void *bp) {
-    if (bp == 0)
-		return;
-	size_t size = GET_SIZE(HDRP(bp));
+
+	if (bp == 0) return;
+  	size_t size = GET_SIZE(HDRP(bp));
 
 	PUT(HDRP(bp), PACK(size, 0));
 	PUT(FTRP(bp), PACK(size, 0));
@@ -202,32 +205,78 @@ static int aligned(const void *p) {
 void mm_checkheap(int verbose) {
 }
 
-static void *coalesce(void *bp) {
+static void *coalesce(void *bp) 
+{
 	size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
-	size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+	size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));	
 	size_t size = GET_SIZE(HDRP(bp));
 
 	if (prev_alloc && next_alloc) {
 		return bp;
 	}
+
 	else if (prev_alloc && !next_alloc) {
 		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
 		PUT(HDRP(bp), PACK(size, 0));
 		PUT(FTRP(bp), PACK(size, 0));
 	}
+
 	else if(!prev_alloc && next_alloc) {
-		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+		size += GET_SIZE(HDRP(PREV_BLKP(bp)));
 		PUT(FTRP(bp), PACK(size, 0));
 		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
 		bp = PREV_BLKP(bp);
 	}
+
 	else {
-		size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
+		size += GET_SIZE(HDRP(PREV_BLKP(bp))) + 
 			GET_SIZE(FTRP(NEXT_BLKP(bp)));
 		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
 		PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
 		bp = PREV_BLKP(bp);
 	}
-
 	return bp;
+}
+
+static void *extend_heap(size_t words)
+{
+	char *bp;
+	size_t size;
+
+	size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
+	if ((long)(bp = mem_sbrk(size)) == -1)
+			return NULL;
+
+	PUT(HDRP(bp), PACK(size, 0));
+	PUT(FTRP(bp), PACK(size, 0));
+	PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+	return coalesce(bp);
+	}
+
+static void *find_fit(size_t asize) {
+	void *bp;
+
+	for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+		if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+			return bp;
+		}
+	}
+	return NULL;
+}
+
+static void place(void *bp, size_t asize) {
+	size_t csize = GET_SIZE(HDRP(bp));
+
+	if ((csize - asize) >= (2 * DSIZE)) {
+		PUT(HDRP(bp), PACK(asize, 1));
+		PUT(FTRP(bp), PACK(asize, 1));
+		bp = NEXT_BLKP(bp);
+		PUT(HDRP(bp), PACK(csize - asize, 0));
+		PUT(FTRP(bp), PACK(csize - asize, 0));
+	}
+	else {
+		PUT(HDRP(bp), PACK(csize, 1));
+		PUT(FTRP(bp), PACK(csize, 1));
+	}
 }
