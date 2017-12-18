2e808a59249e6ceb1a9f3f0ae140d4dadcf66755
jongkukpark <trly12@cnu.ac.kr>
2017-12-18 18:24:35 +0900

    3

--- mm-explicit.c
+++ mm-explicit.c
@@ -235,16 +235,25 @@ inline void *extend_heap(size_t words) {
 
 static void place(void *bp, size_t asize) {
 	size_t csize = GET_SIZE(HDRP(bp));
-
-	if ((csize - asize) >= (2 * DSIZE)) {
+	void *next_free_block = NEXT_FREE_BLKP(bp);
+	void *prev_free_block = PREV_FREE_BLKP(bp); 
+	if ((csize - asize) >= (3 * DSIZE)) {
 		PUT(HDRP(bp), PACK(asize, 1));
 		PUT(FTRP(bp), PACK(asize, 1));
 		bp = NEXT_BLKP(bp);
 		PUT(HDRP(bp), PACK(csize - asize, 0));
 		PUT(FTRP(bp), PACK(csize - asize, 0));
+		
+		PUT(NEXT_FREE_BLKP(bp), next_free_block);
+		PUT(PREV_FREE_BLKP(bp), prev_free_block);
+		PUT(PREV_FREE_BLKP(next_free_block), bp);
+		PUT(NEXT_FREE_BLKP(prev_free_block), bp);
 	}
 	else {
 		PUT(HDRP(bp), PACK(csize, 1));
 		PUT(FTRP(bp), PACK(csize, 1));
+		
+		PUT(NEXT_FREE_BLKP(prev_free_block), prev_free_block);
+		PUT(PREV_FREE_BLKP(next_free-block), next_free_block);
 	}
 }
